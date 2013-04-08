6c07b90db71f7d1d9946a6ec2f7c7b001e480c55
Theoxylo <theoxylo@yahoo.com>
2013-04-08 19:25:04 -0400

    now deleting unused cells for fewer forEach callback; faster?

--- life-engine.js
+++ life-engine.js
@@ -72,6 +72,17 @@ module.exports = function createInstance(game, opts) {
     tickTime *= 1.1
   }
 
+  function getEmptyNeighbor(x, z, dx, dz) { // candidate for new cell if enough live neighbors
+    var x_coord = (x + dx) % boardSize
+    if (x_coord < 0) x_coord += boardSize
+
+    var z_coord = (z + dz) % boardSize
+    if (z_coord < 0) z_coord += boardSize
+
+    if (cells[x_coord * boardSize + z_coord]) return false // there is a live neighbor there, not empty
+    return  { x: x_coord, z: z_coord, on: false } // return inactive cell, candidate for activation
+  }
+
   function tick(dt) { 
     if (paused) return;
     timeSinceTick += dt
@@ -79,17 +90,26 @@ module.exports = function createInstance(game, opts) {
       timeSinceTick = 0
 
       // remove inactive cells
-      cells = cells.map(function (cell) { 
-        return cell && cell.on ? cell : null 
+      //cells = cells.map(function (cell, index, array) { 
+        //return cell && cell.on ? cell : null 
+      //})
+      cells.forEach(function (cell, index, array) { 
+        if (!cell || !cell.on) delete array[index] // remove inactive cells
       })
 
       var possibleNewCells = []
 
+      var cellCount = 0;
+      
       cells.forEach(function (cell) {
-        if (!cell) return
+        cellCount++
+        if (!cell) {
+          console.log("found bad cell in 'cells' array even after cleanup")
+          return;
+        }
         // check each of 8 neighbors:
         // if a neighbor is active, increment counter
-        // otherwise, save for later to check for new cell
+        // otherwise, save for later to check for new cell birth
         var count = 0 // number of live neighbors
         for (var dx = -1; dx < 2; dx++) {
           for (var dz = -1; dz < 2; dz++) {
@@ -101,34 +121,28 @@ module.exports = function createInstance(game, opts) {
         }
         if (count < 2 || count > 3) cell.on = false
       })
+      console.log("live cell count: " + cellCount)
 
-      possibleNewCells.map(function (cell) {
+      // 
+      possibleNewCells.forEach(function (cell, index, array) {
         var count = 0 // number of live neighbors
         for (var dx = -1; dx < 2; dx++) {
           for (var dz = -1; dz < 2; dz++) {
             if (dx === 0 && dz === 0) continue // self
-            var empty = getEmptyNeighbor(cell.x, cell.z, dx, dz)
-            if (!empty) count++
+            if (!getEmptyNeighbor(cell.x, cell.z, dx, dz)) count++ // count liviing neighbors
           }
         }
-        if (count === 3) return cell // a new cell is born
-        return null
-      }).forEach(function (cell) {
-          if (!cell) return
-          cell.on = true
-          addCell(cell)
+        if (count !== 3) delete array[index] // this cell didn't make it
       })
-
-      function getEmptyNeighbor(x, z, dx, dz) { // candidate for new cell if enough live neighbors
-        var x_coord = (x + dx) % boardSize
-        if (x_coord < 0) x_coord += boardSize
-
-        var z_coord = (z + dz) % boardSize
-        if (z_coord < 0) z_coord += boardSize
-
-        if (cells[x_coord * boardSize + z_coord]) return false // there is a live neighbor there
-        return  { x: x_coord, z: z_coord, on: false } // return inactive cell, candidate for activation
-      }
+      
+      possibleNewCells.forEach(function (cell) {
+        if (!cell) {
+          console.log("found bad cell in 'possibleNewCells' array even after cleanup")
+          return;
+        }
+        cell.on = true
+        addCell(cell)
+      })
 
       updateVoxels()
     }
@@ -136,7 +150,10 @@ module.exports = function createInstance(game, opts) {
 
   function updateVoxels() {
     cells.forEach(function (cell) { 
-      if (!cell) return
+      if (!cell) {
+        console.log("found bad cell in 'cells' array during updateVoxels")
+        return;
+      }
       var pos = [boardPos[0] + cell.x, boardPos[1], boardPos[2] + cell.z] 
       game.setBlock(pos, cell.on ? on_material : off_material)
     })
